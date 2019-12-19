444c0057764b438c5097126829ff8fcb976b1e1b
林榕 <linrong@lilithgames.com>
2019-12-19 11:22:13 +0800

    init repo

---
+++ src/pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="page">
+    {{userInfo.name}}
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  async mounted(){
+    console.log('mounted')
+    
+    const api = await this.$request('api')
+    
+  },
+  computed:mapState([
+    'userInfo'
+  ]),
+
+  components: {
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test'}
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+</style>
